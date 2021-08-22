ebf7c02506b2dfdc25337cfaecd529e98eab2cba
Hadi <abdlhadi1999apdo@gmail.com>
2021-08-22 15:14:37 +0200

    fix: web support

--- lib/src/dialog_factory.dart
+++ lib/src/dialog_factory.dart
@@ -1,4 +1,5 @@
-import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:universal_io/io.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -17,11 +18,14 @@ abstract class DialogFactory {
     if (type == DialogStyle.cupertino) {
       return _CupertinoDialogFactory();
     }
-
-    if (Platform.isAndroid) {
+    if (kIsWeb) {
       return _MaterialDialogFactory();
     } else {
-      return _CupertinoDialogFactory();
+      if (Platform.isAndroid) {
+        return _MaterialDialogFactory();
+      } else {
+        return _CupertinoDialogFactory();
+      }
     }
   }
 
@@ -38,12 +42,11 @@ class _MaterialDialogFactory implements DialogFactory {
     return AlertDialog(
       title: Text(title),
       content: Text(content),
-      actions: actions.map(
-          (action) => TextButton(
-            onPressed: action.handler,
-            child: Text(action.title.toUpperCase())
-          )
-      ).toList(growable: false),
+      actions: actions
+          .map((action) => TextButton(
+              onPressed: action.handler,
+              child: Text(action.title.toUpperCase())))
+          .toList(growable: false),
     );
   }
 
@@ -68,14 +71,14 @@ class _CupertinoDialogFactory implements DialogFactory {
     return CupertinoAlertDialog(
       title: Text(title),
       content: Text(content),
-      actions: actions.map(
-          (action) => CupertinoDialogAction(
-            onPressed: action.handler,
-            child: Text(action.title),
-            isDefaultAction: action.isDefault,
-            isDestructiveAction: action.isDestructive,
-          )
-      ).toList(growable: false),
+      actions: actions
+          .map((action) => CupertinoDialogAction(
+                onPressed: action.handler,
+                child: Text(action.title),
+                isDefaultAction: action.isDefault,
+                isDestructiveAction: action.isDestructive,
+              ))
+          .toList(growable: false),
     );
   }
 
